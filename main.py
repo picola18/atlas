35ee243177fb378b9f36dbe7aaa7b0fb7f70d4b6
picola <157052926+picola18@users.noreply.github.com>
2024-02-29 16:26:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,3 @@ endereco_email = mail.group(1)
 sujeito = f'{subject}'
 
 
-sys_msg = "you are a useful assistent, gather the bits of info you can find in the message and answer it in a professional, thankful, manner, either in portuguese or in english, depending on the user message."
-
-
